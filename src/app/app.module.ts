3c1f4b8d4936194117a18ad6f7f7c261449fa733
laguna99999 <lookatme99999@yahoo.com>
2019-08-19 12:19:17 +1000

    Starting components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { PaginationComponent } from './component/pagination/pagination.component';
+import { ListComponent } from './component/list/list.component';
+import { CategoryComponent } from './component/category/category.component';
+import { ConfirmComponent } from './component/confirm/confirm.component';
+import { CustomizeComponent } from './component/customize/customize.component';
+import { EndComponent } from './component/end/end.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PaginationComponent,
+    ListComponent,
+    CategoryComponent,
+    ConfirmComponent,
+    CustomizeComponent,
+    EndComponent
   ],
   imports: [
     BrowserModule
