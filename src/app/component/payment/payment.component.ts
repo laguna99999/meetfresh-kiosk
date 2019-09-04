c0fa8e98a0e609c3aed318a3d6a8b208fa808b08
laguna99999 <lookatme99999@yahoo.com>
2019-09-04 23:59:16 +1000

    Payment page complete.

--- src/app/component/payment/payment.component.ts
+++ src/app/component/payment/payment.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
-  selector: 'app-payment',
-  templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.css']
+    selector: 'app-payment',
+    templateUrl: './payment.component.html',
+    styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        private router: Router
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    public gift(){
+        this.router.navigate(['/thankyou']);
+    }
+    public credit(){
+        this.router.navigate(['/thankyou']);
+    }
+    public quick(){
+        this.router.navigate(['/thankyou']);
+    }
+
+
+    public back(){
+        this.router.navigate(['/member']);
+    }
+    public home(){
+        this.router.navigate(['/home']);
+    }
 }
